9366baa8c4f86331f7765ec3684823677c6c56ec
wu-117 <65855798+wu-117@users.noreply.github.com>
2020-09-08 01:37:17 +0800

    modifypass

--- frontend/src/views/Student/StudentChangePass.vue
+++ frontend/src/views/Student/StudentChangePass.vue
@@ -31,14 +31,14 @@
 
     <el-main style="width: 50%" >
         <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm" >
+            <el-form-item label="旧密码" prop="oldpass">
+                <el-input v-model.number="ruleForm.oldpass"></el-input>
+            </el-form-item>
             <el-form-item label="密码" prop="pass">
                 <el-input type="password" v-model="ruleForm.pass" autocomplete="off" ></el-input>
             </el-form-item>
             <el-form-item label="确认密码" prop="checkPass">
                 <el-input type="password" v-model="ruleForm.checkPass" autocomplete="off" ></el-input>
-            </el-form-item>
-            <el-form-item label="年龄" prop="age">
-                <el-input v-model.number="ruleForm.age"></el-input>
             </el-form-item>
             <el-form-item>
                 <el-button type="primary" @click="submitForm('ruleForm')">提交</el-button>
@@ -55,22 +55,6 @@
 <script>
     export default {
         data() {
-            var checkAge = (rule, value, callback) => {
-                if (!value) {
-                    return callback(new Error('年龄不能为空'));
-                }
-                setTimeout(() => {
-                    if (!Number.isInteger(value)) {
-                        callback(new Error('请输入数字值'));
-                    } else {
-                        if (value < 18) {
-                            callback(new Error('必须年满18岁'));
-                        } else {
-                            callback();
-                        }
-                    }
-                }, 1000);
-            };
 
             var validatePass = (rule, value, callback) => {
                 if (value === '') {
@@ -97,7 +81,7 @@
                 ruleForm: {
                     pass: '',
                     checkPass: '',
-                    age: ''
+                    oldpass: ''
                 },
                 rules: {
                     pass: [
@@ -106,9 +90,6 @@
                     checkPass: [
                         { validator: validatePass2, trigger: 'blur' }
                     ],
-                    age: [
-                        { validator: checkAge, trigger: 'blur' }
-                    ]
                 }
             };
         },
